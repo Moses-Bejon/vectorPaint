d0ba2dcbe4aa81868d6cf15b261b6ace6d862aa4
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-01-13 19:46:27 +0000

    Update completed extension

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ root = Tk()
 
 canvas = VectorPaintView.myCanvas(root)
 buttonBar = VectorPaintView.ButtonBar(root,canvas,50)
+canvas.set_scaler(buttonBar.getBorderScale)
 
 canvas.frame.pack(side=RIGHT, fill=BOTH, expand=True)
 buttonBar.frame.pack(side=LEFT, fill=Y)
