d55c1e184822090eaef484eddea438834bb494b6
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-01-13 20:13:23 +0000

    Update removed a bad smell

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ root = Tk()
 
 canvas = VectorPaintView.myCanvas(root)
 buttonBar = VectorPaintView.ButtonBar(root,canvas,50)
-canvas.set_scaler(buttonBar.getBorderScale)
 
 canvas.frame.pack(side=RIGHT, fill=BOTH, expand=True)
 buttonBar.frame.pack(side=LEFT, fill=Y)
