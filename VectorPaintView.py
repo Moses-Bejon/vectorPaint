456b9fc04f6c2169c8d183dbbb68b174349fefa1
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-01-13 19:45:29 +0000

    Update completed extension

--- VectorPaintView.py
+++ VectorPaintView.py
@@ -14,15 +14,28 @@ class ButtonBar:
 
         self.frame = Frame(self.__root_frame, width=width)
 
-        self.__buttons = [[Label(self.frame,text="fill colour"), Button(self.frame, text="Red", command=lambda: self.__controls.set_colour("Red")), Button(self.frame, text="Black", command=lambda: self.__controls.set_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_colour("Blue"))],
-                          [Label(self.frame,text="outline colour"), Button(self.frame, text="Red", command=lambda: self.__controls.set_outline_colour("Red")), Button(self.frame, text="Black", command=lambda: self.__controls.set_outline_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_outline_colour("Blue"))],
+        self.__buttons = [[Label(self.frame,text="fill colour"), Button(self.frame, text="Red", command=lambda: self.__controls.set_colour("Red")), Button(self.frame, text="Black", command=lambda: self.__controls.set_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_colour("Blue")),Button(self.frame, text="White", command=lambda: self.__controls.set_colour("White"))],
+                          [Label(self.frame,text="outline colour"), Button(self.frame, text="Red", command=lambda: self.__controls.set_outline_colour("Red")), Button(self.frame, text="Black", command=lambda: self.__controls.set_outline_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_outline_colour("Blue")),Button(self.frame, text="White", command=lambda: self.__controls.set_outline_colour("White"))],
+                          [Label(self.frame, text="back ground colour"),Button(self.frame, text="Red", command=lambda: self.__controls.set_background_colour("Red")),Button(self.frame, text="Black",command=lambda: self.__controls.set_background_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_background_colour("Blue")),Button(self.frame, text="White",command=lambda: self.__controls.set_background_colour("White"))],
                           [Label(self.frame,text="shape"), Button(self.frame, text="Line", command=lambda: self.__controls.set_shape(Line)),Button(self.frame, text="Rectangle", command=lambda: self.__controls.set_shape(Rectangle))]]
+
+        width = len(self.__buttons[0])
+        height = len(self.__buttons)
+
         for column,buttons in enumerate(self.__buttons):
             for row,button in enumerate(buttons):
                 button.grid(row=row,column=column)
             self.frame.columnconfigure(column)
-        for row in range(len(self.__buttons[0])):
+        for row in range(width):
             self.frame.rowconfigure(row)
+
+        self.__borderScaler = Scale(self.frame,orient=HORIZONTAL)
+
+        Label(self.frame,text="shape border size:").grid(row=height+1,column=0)
+        self.__borderScaler.grid(row=height+1,column=1,columnspan=width-1,sticky="NSEW")
+
+    def getBorderScale(self):
+        return self.__borderScaler.get()
 
 class myCanvas:
     def __init__(self, root_frame):
@@ -39,10 +52,10 @@ class myCanvas:
         self.frame.bind("<ButtonRelease-1>", self.stop_draw)
         self.frame.bind("<B1-Motion>", self.continue_draw)
 
-        self.resize()
-
-    def resize(self):
-        pass
+    def set_scaler(self, scaler):
+        self.__scaler = scaler
+    def get_outline_scale(self):
+        return self.__scaler()
 
     def set_colour(self, colour):
         self.__currColour = colour
@@ -50,11 +63,14 @@ class myCanvas:
     def set_outline_colour(self,colour):
         self.__currOutlineColour = colour
 
+    def set_background_colour(self,colour):
+        self.frame.config(bg=colour)
+
     def set_shape(self, shape):
         self.__currShape = shape
 
     def start_draw(self, event):
-        self.__currObject = self.__currShape(self.frame,event,self.__currColour,self.__currOutlineColour)
+        self.__currObject = self.__currShape(self.frame,event,self.__currColour,self.__currOutlineColour,self.get_outline_scale())
 
     def continue_draw(self, event):
         self.__currObject.continue_draw(event)
