28b40825e8ba83610e3a4bbdd857e4932336c3f4
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-01-13 17:34:18 +0000

    Update added outline colours

--- VectorPaintView.py
+++ VectorPaintView.py
@@ -12,9 +12,11 @@ class ButtonBar:
         self.__root_frame = root_frame
         self.__controls = controls
 
-        self.frame = Frame(self.__root_frame, bg="Gray", width=width)
+        self.frame = Frame(self.__root_frame, width=width)
 
-        self.__buttons = [[Button(self.frame, text="Black", command=lambda: self.__controls.set_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_colour("Blue"))],[Button(self.frame, text="Line", command=lambda: self.__controls.set_shape(Line)),Button(self.frame, text="Rectangle", command=lambda: self.__controls.set_shape(Rectangle))]]
+        self.__buttons = [[Label(self.frame,text="fill colour"), Button(self.frame, text="Red", command=lambda: self.__controls.set_colour("Red")), Button(self.frame, text="Black", command=lambda: self.__controls.set_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_colour("Blue"))],
+                          [Label(self.frame,text="outline colour"), Button(self.frame, text="Red", command=lambda: self.__controls.set_outline_colour("Red")), Button(self.frame, text="Black", command=lambda: self.__controls.set_outline_colour("Black")),Button(self.frame, text="Blue", command=lambda: self.__controls.set_outline_colour("Blue"))],
+                          [Label(self.frame,text="shape"), Button(self.frame, text="Line", command=lambda: self.__controls.set_shape(Line)),Button(self.frame, text="Rectangle", command=lambda: self.__controls.set_shape(Rectangle))]]
         for column,buttons in enumerate(self.__buttons):
             for row,button in enumerate(buttons):
                 button.grid(row=row,column=column)
@@ -27,6 +29,7 @@ class myCanvas:
         self.__root_frame = root_frame
 
         self.__currColour = "Red"
+        self.__currOutlineColour = "Red"
         self.__currShape = Line
         self.__currPlace = None
 
@@ -44,11 +47,14 @@ class myCanvas:
     def set_colour(self, colour):
         self.__currColour = colour
 
+    def set_outline_colour(self,colour):
+        self.__currOutlineColour = colour
+
     def set_shape(self, shape):
         self.__currShape = shape
 
     def start_draw(self, event):
-        self.__currObject = self.__currShape(self.frame,event,self.__currColour)
+        self.__currObject = self.__currShape(self.frame,event,self.__currColour,self.__currOutlineColour)
 
     def continue_draw(self, event):
         self.__currObject.continue_draw(event)
