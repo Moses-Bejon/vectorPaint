051063bd98dac1ec585c1f46a5a625bf7091ac3d
Moses-Bejon <138283384+Moses-Bejon@users.noreply.github.com>
2024-02-07 13:57:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
 from tkinter import *
-import VectorPaintView
-
-
+from VectorPaintController import VectorPaintController
+from VectorPaintModel import VectorPaintModel
+from VectorPaintView import *
 
 root = Tk()
+root.title("Vector paint project")
 
-canvas = VectorPaintView.myCanvas(root)
-buttonBar = VectorPaintView.ButtonBar(root,canvas,50)
+model = VectorPaintModel()
+controller = VectorPaintController(model)
 
-canvas.frame.pack(side=RIGHT, fill=BOTH, expand=True)
-buttonBar.frame.pack(side=LEFT, fill=Y)
+canvas = myCanvas(root, controller,model) # called it mycanvas because canvas already exists :(
+tree = treeView(root, controller,model) # Shows the clickable buttons for moving around the tree
+buttonBar = canvas.buttonBar # the canvas is composed of the button bar, so I want to grid the canvas's button bar
+menuBar = MenuBar(root,controller) # save, save as and load
+
+menuBar.frame.grid(column=0,row=0,columnspan=3,sticky="E")
+canvas.frame.grid(column=2,row=1,sticky=NSEW)
+tree.frame.grid(column=0,row=1,sticky=N)
+buttonBar.grid(column=1,row=1,sticky=N)
+root.grid_rowconfigure(1,weight=1)
+root.grid_columnconfigure(2,weight=1)
 
 root.mainloop()
